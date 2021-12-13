a9928a2c4bb3dfb5817b832648071a2a287a9c22
aijaindriksone <indriksoneaija@gmail.com>
2021-12-13 13:37:59 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+var word = "";
+var wordGuess = [];
+var wrongGuess = [];
+var guessBomb = 0;
+var winCount = 1;
+var guess = "";
+var dif = 0;
+
+function lim1() {
+    dif = 1;
+    document.getElementById('saktButton').style.display='block';
+    document.getElementById('izvelieslimeni').style.display='none';
+}
